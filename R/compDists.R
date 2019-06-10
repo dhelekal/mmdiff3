d07c27968c862be764764d2dc10a65646ce4ce05
David Helekal <david.helekal@gmail.com>
2019-06-10 17:04:24 +0100

    modifications to config, lower negative contrast sampling

--- R/compDists.R
+++ R/compDists.R
@@ -156,7 +156,7 @@ compDists <- function(MD, dist.method='MMD',sigma=NULL,CompIDs=NULL,
     names(NegativeContrast) <- colnames(Counts)
     for (i in 1:ncol(Counts)){
       N <- median(Counts[,i]/width(Peaks))*width(Peaks)
-      NegativeContrast[[i]] <- N
+      NegativeContrast[[i]] <- N/5
     }
     
     message('estimating sigma...')
